bc7c4a2de11d2327edd36bc3aa0c0afec0a89be6
Minsoo Choo <minsoochoo0122@proton.me>
2024-04-01 11:41:38 -0400

    Initial Commit

---
+++ Venus/VenusApp.swift
@@ -0,0 +1,17 @@
+//
+//  VenusApp.swift
+//  Venus
+//
+//  Created by Minsoo Choo on 2024-04-01.
+//
+
+import SwiftUI
+
+@main
+struct VenusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
